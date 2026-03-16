d8ff506270ce8672f368ac355c2e850c6264cb8b
Angel Larios <angel.larios@ucn.edu.ni>
2026-03-16 10:40:25 -0600

    se creo la consulta para mostrar los productos en el DataTable

---
+++ Ejemplos/NCAPAS-PRODUCTOS/CapaDatos/CDProductos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaDatos
+{
+    public class CDProductos
+    {
+        private CD_Conexion conexion = new CD_Conexion();
+
+        SqlDataReader leer;
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
+
+
+        public DataTable Mostrar()
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "SELECT Id, Nombre, Descripcion, Marca, Precio, Stock FROM Productos";
+            leer = comando.ExecuteReader();
+            tabla.Load(leer);
+            return tabla;
+        }
+    }
+}
---
+++ Ejemplos/NCAPAS-PRODUCTOS/CapaNegocio/CNProductos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+
+
+namespace CapaNegocio
+{
+    internal class CNProductos
+    {
+        private CDProductos objetoCD = new CDProductos();
+
+        public DataTable MostrarProducto()
+        {
+            DataTable dt = new DataTable();
+            dt = objetoCD.Mostrar();
+            return dt;
+        }
+    }
+}
