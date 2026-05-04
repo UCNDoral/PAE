3916967745769cde7d1c28373f7536dbaf082e72
Angel Larios <angel.larios@ucn.edu.ni>
2026-05-04 10:17:39 -0600

    UI de BancoSimulador

---
+++ Ejemplos/BancoSimulador/BancoSimulador/Form1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BancoSimulador
+{
+    public partial class Form1 : Form
+    {
+      
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+        }
+
+    }
+}
